5791ad5fec8a38e53e4cfa7abf57aa60fa9b07be
Artem Grigorash <artem.s.grigorash@gmail.com>
2023-01-13 19:52:32 +0300

    intents

--- app/src/main/java/com/example/playlistmakerag/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakerag/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmakerag
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,6 +15,34 @@ class SettingsActivity : AppCompatActivity() {
         val settingsBack = findViewById<ImageView>(R.id.settings_back)
         settingsBack.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val settingsShare = findViewById<FrameLayout>(R.id.settings_share)
+        settingsShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            val url = "https://practicum.yandex.ru/android-developer/"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, url)
+            shareIntent.type = "text/plain"
+            startActivity(shareIntent)
+        }
+
+        val settingsMail = findViewById<FrameLayout>(R.id.settings_mail)
+        settingsMail.setOnClickListener{
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val massageTheme = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.data = Uri.parse("mailto:")
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("artem.s.grigorash@gmail.com"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, massageTheme)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(shareIntent)
+        }
+
+        val settingsAgreement = findViewById<FrameLayout>(R.id.settinds_agreement)
+        settingsAgreement.setOnClickListener{
+            val url = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val intent = Intent(Intent.ACTION_VIEW, url)
             startActivity(intent)
         }
     }
