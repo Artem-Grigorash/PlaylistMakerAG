ede1bc65ff0b2f15e625eaf1df10092c65e1ab4d
Artem Grigorash <artem.s.grigorash@gmail.com>
2023-01-17 20:38:21 +0300

    search

--- app/src/main/java/com/example/playlistmakerag/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakerag/SearchActivity.kt
@@ -30,7 +30,7 @@ class SearchActivity : AppCompatActivity() {
 
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                // empty
+                clearButton.visibility = View.GONE
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
