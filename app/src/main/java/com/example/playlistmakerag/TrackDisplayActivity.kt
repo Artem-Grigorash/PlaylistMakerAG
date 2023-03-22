28118a701cccbebb2e0a81b2ea4f5cf4e02b987e
Artem Grigorash <artem.s.grigorash@gmail.com>
2023-03-22 17:35:41 +0300

    after review final

--- app/src/main/java/com/example/playlistmakerag/TrackDisplayActivity.kt
+++ app/src/main/java/com/example/playlistmakerag/TrackDisplayActivity.kt
@@ -1,7 +1,5 @@
 package com.example.playlistmakerag
 
-import android.content.Intent
-import android.graphics.Picture
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
@@ -10,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -25,7 +22,6 @@ class TrackDisplayActivity : AppCompatActivity() {
     private lateinit var year : TextView
     private lateinit var genre : TextView
     private lateinit var country : TextView
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
