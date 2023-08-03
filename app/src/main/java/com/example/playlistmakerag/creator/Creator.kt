6c42f29ec22efd592a4116206c1e5a6da4e4aaa7
Artem Grigorash <artem.s.grigorash@gmail.com>
2023-08-03 18:12:42 +0300

    После ревью 2 (final)

--- app/src/main/java/com/example/playlistmakerag/creator/Creator.kt
+++ app/src/main/java/com/example/playlistmakerag/creator/Creator.kt
@@ -5,6 +5,11 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import com.example.playlistmakerag.app.PREFERENCES
+import com.example.playlistmakerag.player.data.player.Player
+import com.example.playlistmakerag.search.data.Retrofit
+import com.example.playlistmakerag.search.data.SearchHistory
+import com.example.playlistmakerag.settings.data.SwitchTheme
+import com.example.playlistmakerag.sharing.data.ExternalNavigator
 
 object Creator {
     private lateinit var application: Application
@@ -20,4 +25,25 @@ object Creator {
     fun provideContext(): Context {
         return application.applicationContext
     }
+
+    fun getPlayer(url: String): Player {
+        return Player(url)
+    }
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit()
+    }
+
+    fun getSwitchTheme(): SwitchTheme {
+        return SwitchTheme()
+    }
+
+    fun getHistory(): SearchHistory {
+        return SearchHistory()
+    }
+
+    fun getNavigator(): ExternalNavigator {
+        return ExternalNavigator()
+    }
+
 }
