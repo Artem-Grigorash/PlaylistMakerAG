6539119d154ec2f88b196f27b24291af2ebd3e4a
Artem Grigorash <artem.s.grigorash@gmail.com>
2023-08-15 22:24:42 +0300

    первые изменения

---
+++ app/src/main/java/com/example/playlistmakerag/mediateka/FragmentFavoriteTracks.kt
@@ -0,0 +1,49 @@
+package com.example.playlistmakerag.mediateka
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.marginTop
+import androidx.fragment.app.Fragment
+import com.example.playlistmakerag.R
+import com.example.playlistmakerag.databinding.FragmentFavoriteTracksBinding
+
+
+class FragmentFavoriteTracks : Fragment() {
+
+    companion object {
+        private const val NUMBER = "number"
+
+        fun newInstance(number: Int) = FragmentFavoriteTracks().apply {
+            arguments = Bundle().apply {
+                putInt(NUMBER, number)
+            }
+        }
+    }
+
+    private lateinit var binding: FragmentFavoriteTracksBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentFavoriteTracksBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        when(requireArguments().getInt(NUMBER)) {
+            1 -> {
+                binding.placeholderMessage.text = "Ваша медиатека пуста"
+                binding.placeholder.setImageResource(R.drawable.tracks_placeholder_nf)
+                binding.cleanHistoryButton.visibility = View.GONE
+            }
+            2 -> {
+                binding.placeholderMessage.text = "Вы не создали\nни одного плейлиста"
+                binding.placeholder.setImageResource(R.drawable.tracks_placeholder_nf)
+                binding.cleanHistoryButton.visibility = View.VISIBLE
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/playlistmakerag/mediateka/MediatekaActivity.kt
+++ app/src/main/java/com/example/playlistmakerag/mediateka/MediatekaActivity.kt
@@ -2,11 +2,34 @@ package com.example.playlistmakerag.mediateka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.playlistmakerag.R
+import com.example.playlistmakerag.databinding.ActivityMediatekaBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MediatekaActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMediatekaBinding
+
+    private lateinit var tabMediator: TabLayoutMediator
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mediateka)
+        binding = ActivityMediatekaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.viewPager.adapter = TracksViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabMediator = TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            when(position) {
+                0 -> tab.text = "Избранные треки"
+                1 -> tab.text = "Плейлисты"
+            }
+        }
+        tabMediator.attach()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        tabMediator.detach()
+    }
+
 }
