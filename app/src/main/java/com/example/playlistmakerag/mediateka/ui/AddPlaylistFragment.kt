8993dc37f19666e2fb0178bd55ffa00fdc7ba998
Artem Grigorash <artem.s.grigorash@gmail.com>
2023-10-10 22:29:30 +0300

    первые изменения по первому пункту

--- app/src/main/java/com/example/playlistmakerag/mediateka/ui/AddPlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmakerag/mediateka/ui/AddPlaylistFragment.kt
@@ -8,7 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.playlistmakerag.R
 import com.example.playlistmakerag.databinding.FragmentAddPlaylistBinding
 import com.example.playlistmakerag.databinding.FragmentSearchBinding
@@ -20,38 +22,44 @@ class AddPlaylistFragment : Fragment() {
     private lateinit var nameEditText: EditText
     private lateinit var descriptionEditText: EditText
     private lateinit var saveButton: Button
+    private lateinit var backButton: ImageView
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_add_playlist, container, false)
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentAddPlaylistBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setViews()
+        saveButton.isClickable = false
+        saveButton.isEnabled = false
+        backButton.setOnClickListener {
+            findNavController().navigate(R.id.action_addPlaylistFragment_to_mediatekaFragment)
+        }
         val textWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                saveButton.isClickable = s.isNullOrEmpty()
-//                saveButton.background = if (s?.isEmpty() == true) R.color.background_main else R.color.
+                saveButton.isClickable = s?.isNotEmpty() == true
+                saveButton.isEnabled = s?.isNotEmpty() == true
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                saveButton.isClickable = s.isNullOrEmpty()
+                saveButton.isClickable = s?.isNotEmpty() == true
+                saveButton.isEnabled = s?.isNotEmpty() == true
             }
 
             override fun afterTextChanged(s: Editable?) {
-                saveButton.isClickable = s.isNullOrEmpty()
+                saveButton.isClickable = s?.isNotEmpty() == true
+                saveButton.isEnabled = s?.isNotEmpty() == true
             }
         }
         nameEditText.addTextChangedListener(textWatcher)
-        descriptionEditText.addTextChangedListener(textWatcher)
     }
 
     private fun setViews(){
         nameEditText = binding.editName
         descriptionEditText = binding.editDescription
-        saveButton=binding.saveButton
+        saveButton = binding.saveButton
+        backButton = binding.backButton
     }
 }
