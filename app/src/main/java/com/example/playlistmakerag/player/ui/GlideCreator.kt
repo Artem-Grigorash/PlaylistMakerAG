578e95d9ce21fcfae25d8f4857271fc9b31a76fa
Artem Grigorash <artem.s.grigorash@gmail.com>
2024-01-07 00:24:38 +0200

    after review first fixes

--- app/src/main/java/com/example/playlistmakerag/player/ui/GlideCreator.kt
+++ app/src/main/java/com/example/playlistmakerag/player/ui/GlideCreator.kt
@@ -2,6 +2,7 @@ package com.example.playlistmakerag.player.ui
 
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmakerag.R
 import com.example.playlistmakerag.player.domain.models.Track
@@ -11,7 +12,7 @@ class GlideCreator {
         Glide.with(picture)
             .load(track.artworkUrl100.replaceAfterLast('/', "512x512bb.jpg"))
             .placeholder(R.drawable.tracks_place_holder)
-            .transform(RoundedCorners(30))
+            .transform(CenterCrop(), RoundedCorners(8))
             .into(picture)
     }
 }
